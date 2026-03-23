03bc218da67189d5daf13d0762e42f5a8c01f46a
thandupsherpa <thandupsherpa153@gmail.com>
2026-03-23 16:35:41 +0530

    User register API (api/auth/register)

--- src/app.js
+++ src/app.js
@@ -6,4 +6,12 @@ const app = express();
 app.use(express.json());
 
 
+/* Require all the routes here */
+const authRouter = require('./routes/auth.routes')
+
+
+/* Using all the routes here*/
+app.use('/api/auth',authRouter)
+
+
 module.exports = app
