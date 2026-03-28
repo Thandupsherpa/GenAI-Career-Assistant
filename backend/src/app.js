a8a056bed2a39d2060fe8f5fb238eda4b506a529
thandupsherpa <thandupsherpa153@gmail.com>
2026-03-28 15:34:44 +0530

    Fixed Cors err & navigate user to / after login successfully

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const cors = require("cors")
 
 const app = express();
 
@@ -7,6 +8,11 @@ const app = express();
 app.use(express.json());
 
 app.use(cookieParser());
+
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true
+}))
 
 
 /* Require all the routes here */
--- frontend/src/features/auth/hooks/useAuth.js
+++ frontend/src/features/auth/hooks/useAuth.js
@@ -4,29 +4,53 @@ import { login,register,logout,getMe } from "../services/auth.api";
 
 export const useAuth = () =>{
     const context = useContext(AuthContext)
-    const {uset,setUser,loading,setLoading} = context
+    const {user,setUser,loading,setLoading} = context
 
     const handleLogin = async({email,password}) =>  {
         setLoading(true)
-        const data = await login({email,password})
-        setUser(data.user)
-        setLoading(false)
+        try{
+            const data = await login({email,password})
+            setUser(data.user)
+
+        }catch(err){
+             console.log(err.response?.data);
+             throw err;
+            
+        }finally{
+            setLoading(false)
+        }
     }
 
     const handleRegister = async({username,email,password}) =>{
         setLoading(true)
-        const data = await register({username,email,password})
-        setUser(data.user)
-        setLoading(true)
+        try{
+            const data = await register({username,email,password})
+            setUser(data.user)
+
+        }catch(err){
+
+        }finally{
+            setLoading(false)
+
+        } 
+        
     }
 
     const handleLogout = async() =>{
         setLoading(true)
-        const data = await logout()
-        setUser(null)
-        setLoading(false)
 
+        try{
+            const data = await logout()
+            setUser(null)
 
+        }catch(err){
+
+        }finally{
+            setLoading(false)
+
+        }
+        
+        
     }
 
     return {user,loading,handleRegister,handleLogin,handleLogout}
